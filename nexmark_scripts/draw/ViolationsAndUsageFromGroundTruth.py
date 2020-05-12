fb90af6b9b4d7a8bb7e27c94e932b887bc8dd217
maoyancan <maoyancan@gmail.com>
2020-05-12 15:31:54 +0800

    update

--- nexmark_scripts/draw/ViolationsAndUsageFromGroundTruth.py
+++ nexmark_scripts/draw/ViolationsAndUsageFromGroundTruth.py
@@ -40,7 +40,7 @@ inputDir = '/home/samza/workspace/flink-testbed/nexmark_scripts/draw/logs/' + fi
 # inputDir = '/home/myc/workspace/SSE-anaysis/src/nexmark_scripts/log/'
 outputDir = 'figures/' + figureName + '/'
 
-keyAverageLatencyFlag = False
+keyAverageLatencyFlag = True
 keyAverageLatencyThreshold = 0.2
 keyLatencyIntervalFlag = False
 calibrateFlag = False
@@ -296,33 +296,23 @@ def operator_ground_truth(jobid):
     with open(stats_logs_path, 'a') as f:
         f.write("%s\t%s\t%d\t%d\t%d\t%s\t%d-%d\t%.15f\n" %
                 (figureName, jobid, retValue[1], retValue[2], retValue[3], retValue[0], minOEs, maxOEs, successRate))
-    # Calculate avg latency
-    if (False):
-        print("Calculate avg lantecy")
-        sumLatency = 0
-        totalTuples = 0
-        for i in range(0, len(substreamLatency)):
-            # print(substreamLatency[i])
-            sumLatency += sum(substreamLatency[i])
-            totalTuples += len(substreamLatency[i])
-
-        avgLatency = sumLatency / float(totalTuples)
-        print('avg latency=', avgLatency)
-
-        # Calculate standard error
-        sumDeviation = 0.0
-        print("Calculate standard deviation")
-        for i in range(0, len(substreamLatency)):
-            for j in range(0, len(substreamLatency[i])):
-                sumDeviation += (((substreamLatency[i][j] - avgLatency) ** 2) / (totalTuples - 1)) ** (0.5)
-        print('Standard deviation=', sumDeviation)
-        print('Standard error=', sumDeviation / (totalTuples) ** (0.5))
 
 
 def app_ground_truth():
     # sum avg latency of two job, and then calculate succ rate
     userLatency = 2000
 
+    appAvglatency = {} # [keygroup -> timeslot -> avglatency]
+    for jobid in substreamAvgLatency:
+        for keygroup in substreamAvgLatency[jobid]:
+            for timeslot in substreamAvgLatency[jobid][keygroup]:
+                if keygroup not in appAvglatency:
+                    appAvglatency[keygroup] = {}
+                if timeslot not in appAvglatency[keygroup]:
+                    appAvglatency[keygroup][timeslot] = 0
+                appAvglatency[keygroup][timeslot] += substreamAvgLatency[jobid][keygroup][timeslot]
+
+    totalTime = 0
     totalViolation = 0
     violationInWarmUp = []
     totalInPeak = []
@@ -332,31 +322,136 @@ def app_ground_truth():
         violationInWarmUp += [0]
         totalInPeak += [0]
         warmUpIntervals[peakI] = [warmUpIntervals[peakI][0] * base / userWindow, warmUpIntervals[peakI][1] * base / userWindow]
-    # xaxes = [warmup * 1000 / userWindow, calculateInterval[-1] * 1000 / userWindow]
-    xaxes = [warmup * 1000 / userWindow, calculateInterval[-1] * 1000 / userWindow]
+    xaxes = [calculateInterval[0] * 1000 / userWindow, calculateInterval[-1] * 1000 / userWindow]
 
-    timeWithoutPeak = (xaxes[1] - xaxes[0] + 1)
+    substreamTime = []
+    substreamViolation = []
+    substreamLatency = []
+    totalViolationSubstream = {}
 
-    appAvglatency = {} # [timeslot -> avglatency]
-    appNmetric = {}
-    for jobid in substreamAvgLatency:
-        for keygroup in substreamAvgLatency[jobid]:
-            for timeslot in substreamAvgLatency[jobid][keygroup]:
-                if timeslot not in appAvglatency:
-                    appAvglatency[timeslot] = 0
-                if timeslot not in appNmetric:
-                    appNmetric[timeslot] = 0
-                appAvglatency[timeslot] += substreamAvgLatency[jobid][keygroup][timeslot]
-                appNmetric[timeslot] += 1
+    # Draw average latency
+    for substream in sorted(appAvglatency):
+        curSubStreamlatency = appAvglatency[substream]
+        print("Calculate substream " + str(substream))
 
-    for time in appAvglatency:
-        appAvglatency[time] = appAvglatency[time] / appNmetric[time]
-        if xaxes[0] <= time <= xaxes[1] and appAvglatency[time] > userLatency:
-            totalViolation += 1
+        # x is time slot, y is avg latency list
+        x = []
+        y = []
 
-    wholeViolation = totalViolation/float(timeWithoutPeak)
-    print('whole graph violation: ', wholeViolation)
-    print('interval: ' , timeWithoutPeak, ' num of violation: ', totalViolation, ' whole graph success rate: ', 1 - wholeViolation)
+        # the total time interval for violation calculation
+        thisTime = (xaxes[1] - xaxes[0] + 1)
+        for peak in range(0, len(warmUpIntervals)):
+            totalInPeak[peak] += (warmUpIntervals[peak][1] - warmUpIntervals[peak][0] + 1)
+
+        # already be avg latency, just need to form the x and y
+        thisViolation = 0
+        thisViolationInterval = []
+        for time in curSubStreamlatency:
+            avgLatency = curSubStreamlatency[time]
+
+            if time not in totalViolationSubstream:
+                totalViolationSubstream[time] = []
+            if avgLatency > 1000 and substream not in totalViolationSubstream[time]:
+                totalViolationSubstream[time].append(substream)
+
+            x += [time]
+            y += [avgLatency]
+            if xaxes[0] <= time <= xaxes[1] and avgLatency > userLatency:
+                thisViolation += 1
+                if len(thisViolationInterval) > 0 and thisViolationInterval[-1][1] == time - 1:
+                    thisViolationInterval[-1][1] = time
+                else:
+                    thisViolationInterval.append([time, time])
+            # Calculate peak interval
+            for i in range(0, len(warmUpIntervals)):
+                if warmUpIntervals[i][0] <= time <= warmUpIntervals[i][1] and avgLatency > userLatency:
+                    violationInWarmUp[i] += 1
+
+        substreamTime += [thisTime]
+        substreamViolation += [thisViolation]
+        percentage = 0.0
+        if thisTime > 0:
+            percentage = thisViolation / float(thisTime)
+        print(str(substream), percentage, thisTime)
+        totalTime += thisTime
+        totalViolation += thisViolation
+        if keyAverageLatencyFlag:
+            print("Draw ", substream, " violation percentage...")
+            outputFile = outputDir + 'app_windowLatency/' + str(substream) + '.png'
+            if not os.path.exists(outputDir + 'app_windowLatency/'):
+                os.makedirs(outputDir + 'app_windowLatency/')
+            legend = ['Window Average Latency']
+            fig = plt.figure(figsize=(16, 9))
+            plt.plot(x, y, 'bs')
+
+            # Add user requirement
+            userLineX = [xaxes[0], xaxes[1]]
+            userLineY = [userLatency, userLatency]
+            userLineC = 'r'
+            plt.plot(userLineX, userLineY, linewidth=3.0, color=userLineC, linestyle='--')
+
+            plt.legend(legend, loc='upper left')
+            # print(arrivalRateT, arrivalRate)
+            plt.grid(True)
+            axes = plt.gca()
+            axes.set_xlim(xaxes)
+            axes.set_ylim([1, 10 ** 6])
+            axes.set_yscale('log')
+            plt.xlabel('Timeslot Index')
+            plt.ylabel('Average Latency')
+            plt.title('Window Average Latency')
+            plt.savefig(outputFile)
+            plt.close(fig)
+
+        # Draw substream violation percetage histogram
+    if (True):
+        print("Draw overall violation percentage figure...")
+
+        outputFile = outputDir + 'app_keyViolationPercentage.png'
+        if not os.path.exists(outputDir):
+            os.makedirs(outputDir)
+
+        legend = ['Violation Percentage']
+        fig = plt.figure(figsize=(32, 18))
+        x = []
+        for i in range(0, len(substreamTime)):
+            x += [substreamViolation[i] / float(substreamTime[i])]
+        bins = np.arange(0, 0.2, 0.01).tolist() + np.arange(0.2, 1, 0.05).tolist()
+        plt.hist(x, bins=bins)
+        axes = plt.gca()
+        axes.set_xticks(bins)
+        axes.set_yticks(np.arange(0, 1000, 50).tolist())
+        plt.grid(True)
+        plt.xlabel('Violation Percentage')
+        plt.ylabel('# of Keys')
+        plt.title('Keys Violation Percentage')
+        plt.savefig(outputFile)
+        plt.close(fig)
+    # run RateAndWindowDelay, and get the stats:
+    avgViolationPercentage = totalViolation / float(totalTime)
+    sumDeviation = 0.0
+    print('avg success rate=', 1 - avgViolationPercentage)
+    print('total violation number=' + str(totalViolation))
+    # print >> f, ('avg success rate=', 1 - avgViolationPercentage)
+    # print >> f, ('total violation number=' + str(totalViolation))
+    violationNotPeak = totalViolation
+    timeNotPeak = totalTime
+    if (totalViolation > 0):
+        for peakI in range(0, len(warmUpIntervals)):
+            print('violation percentage in warm up ' + str(peakI) + ' is ' + str(
+                violationInWarmUp[peakI] / float(totalViolation)) + ', number is ' + str(violationInWarmUp[peakI]))
+            violationNotPeak -= violationInWarmUp[peakI]
+
+            # print >> f, ('violation percentage in warm up ' + str(peakI) + ' is ' + str(
+            #     violationInWarmUp[peakI] / float(totalViolation)) + ', number is ' + str(violationInWarmUp[peakI]))
+
+            timeNotPeak -= totalInPeak[peakI]
+    successRate = 1 - violationNotPeak / float(timeNotPeak)
+    print('Execept warm up avg success rate=', successRate)
+
+    # wholeViolation = totalViolation/float(timeWithoutPeak)
+    # print('whole graph violation: ', wholeViolation)
+    # print('interval: ' , timeWithoutPeak, ' num of violation: ', totalViolation, ' whole graph success rate: ', 1 - wholeViolation)
 
     sumNumOfOEs = {}
     for jobid in numberOfOEsMap:
@@ -371,11 +466,10 @@ def app_ground_truth():
     stats_logs_path = outputDir + 'stats.txt'
     with open(stats_logs_path, 'a') as f:
         f.write("whole graph success rate: %.15f, min-max OEs: %d-%d\n" %
-                (1 - wholeViolation, min(sumNumOfOEs.values()), max(sumNumOfOEs.values())))
+                (successRate, min(sumNumOfOEs.values()), max(sumNumOfOEs.values())))
 
 for jobid in substreamAvgLatency:
     operator_ground_truth(jobid)
-
 
 
 app_ground_truth()
